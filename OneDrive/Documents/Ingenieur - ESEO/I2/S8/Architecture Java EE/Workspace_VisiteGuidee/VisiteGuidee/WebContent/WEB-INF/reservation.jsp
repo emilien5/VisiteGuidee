f283a5df277f4caad759f799b9c1e9d5616fe898
emilien <emilien@DESKTOP-QBOBFB6>
2018-03-18 18:38:30 +0100

    Modification de reservation et ajout lien BDD Visites

--- OneDrive/Documents/Ingenieur - ESEO/I2/S8/Architecture Java EE/Workspace_VisiteGuidee/VisiteGuidee/WebContent/WEB-INF/reservation.jsp
+++ OneDrive/Documents/Ingenieur - ESEO/I2/S8/Architecture Java EE/Workspace_VisiteGuidee/VisiteGuidee/WebContent/WEB-INF/reservation.jsp
@@ -8,6 +8,54 @@
 
 </head>
 <body>
-gvizuvbz
+
+Bienvenue <% String attribut = (String) request.getAttribute("nom");out.println( attribut );%>
+<br>
+Il y a  <% String NbVisites = (String) request.getAttribute("NbVisites");out.println( NbVisites );%> visites.
+<br>
+<br>
+ <table border="1" id="visite">
+            <tr>
+                <th>Type</th>
+                <th>Ville</th>
+                <th>Date</th>
+                <th>Prix</th>
+            </tr>
+            <tbody  >
+                <tr >
+              		<td>
+              		<% String TypeVisite = (String) request.getAttribute("TypeVisite");out.println( TypeVisite );%>
+              		 </td>
+              		 <td>
+              		<% String Ville = (String) request.getAttribute("Ville");out.println( Ville );%>
+              		 </td>
+              		 <td>
+              		 <% String DateVisite = (String) request.getAttribute("DateVisite");out.println( DateVisite );%>
+              		 </td>
+              		 <td>
+              		 <% String PrixVisite = (String) request.getAttribute("PrixVisite");out.println( PrixVisite );%>
+              		 </td>
+               <%--  <% for(int i = 0; i < (int)request.getAttribute("nombreVisites"); i++) { %>
+                    <td>
+                        <% out.println((String)request.getAttribute("TypeVisite")); %>
+                    </td>
+                    <td>
+                        <% out.println((String)request.getAttribute("Ville")); %>
+                    </td>
+                    <td>
+                        <% out.println((String)request.getAttribute("Datevisite")); %>
+                    </td>
+                    <td>
+                        <% out.println((String)request.getAttribute("PrixVisite")); %>
+                    </td>
+                    <% } %> --%>
+                </tr>
+            </tbody>
+        </table>
+<br>
+<br>
+<a href="CreerVisite.jsp?nom=mamalem&motDePasse=network">Proposer une visite</a>
+<br>
+<a href="connexion.jsp">Déconnexion</a>
 </body>
 </html>
--- OneDrive/Documents/Ingenieur - ESEO/I2/S8/Architecture Java EE/Workspace_VisiteGuidee/VisiteGuidee/src/servlets/Connexion.java
+++ OneDrive/Documents/Ingenieur - ESEO/I2/S8/Architecture Java EE/Workspace_VisiteGuidee/VisiteGuidee/src/servlets/Connexion.java
@@ -25,6 +25,12 @@ public class Connexion extends HttpServlet {
     public static final String CHAMP_NOM_UTILISATEUR = "nom";
     public static final String CHAMP_PASS = "motdepasse";
     
+    public static String TypeVisite = "TypeVisite_DEFAULT";
+    public static String Ville = "Ville_DEFAULT";
+    public static String DateVisite = "DateVisite_DEFAULT";
+    public static String PrixVisite = "PrixVisite_DEFAULT";
+    public static String NbVisites = "0";
+
     public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
         /* Affichage de la page d'inscription */
         this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
@@ -40,15 +46,20 @@ public class Connexion extends HttpServlet {
 
         try {
 			boolean valide = informationValide(nomUtilisateur, motDePasse);
+			boolean valide2 = ConnexionBddVisite();
 			
-			if(valide == true) {
+			if(valide == true && valide2 == true) {
 				/* Stockage du résultat et des messages d'erreur dans l'objet request */
 		        request.setAttribute( CHAMP_NOM_UTILISATEUR, nomUtilisateur );
 		        request.setAttribute( CHAMP_PASS, motDePasse );
-		        
+		        request.setAttribute("TypeVisite", TypeVisite);
+		        request.setAttribute("Ville", Ville);
+		    	request.setAttribute("DateVisite", DateVisite);
+		    	request.setAttribute("PrixVisite", PrixVisite);
+		    	request.setAttribute("NbVisites", NbVisites);
 		        /* Transmission de la paire d'objets request/response à notre JSP */
-		        this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
-			} else {
+		        this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );		        
+		    } else {
 				this.getServletContext().getRequestDispatcher( VUE_ERREUR ).forward( request, response );
 			}
 		} catch (Exception e) {
@@ -60,10 +71,26 @@ public class Connexion extends HttpServlet {
     /* Valide le nom d'utilisateur saisi */
     private boolean informationValide( String id, String mp ) throws ClassNotFoundException{
     		Class.forName("com.mysql.jdbc.Driver");
-        try(Connection connexion = DriverManager.getConnection("jdbc:mysql://localhost:8889/bdd_client?user=root&password=root");
+        try(Connection connexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/bdd_client?user=user&password=user");
             Statement statement = connexion.createStatement();
             ResultSet resultat = statement.executeQuery( "SELECT idUtilisateur FROM Utilisateur WHERE nomUtilisateur = '"+ id +"' and motDePasse = '"+ mp +"'" )) {
 	    	    if(resultat.next() != false) {
+                    return true;
+                }
+	    	} catch ( SQLException e ) {
+	    	    /* Gérer les éventuelles erreurs ici */
+	    		e.printStackTrace();
+	    	}
+	    	
+        return false;
+    }
+    
+    private boolean ConnexionBddVisite() throws ClassNotFoundException{
+    	Class.forName("com.mysql.jdbc.Driver");
+        try(Connection connexionVisite = DriverManager.getConnection("jdbc:mysql://localhost:3306/gestionvisites?user=user&password=user");
+            Statement statement = connexionVisite.createStatement();
+            ResultSet resultNbVisite = statement.executeQuery( "SELECT COUNT(idVisite) as nbVisites FROM visite")) {
+	    	    if(resultNbVisite.next() != false) {
                     return true;
                 }
 	    	} catch ( SQLException e ) {
